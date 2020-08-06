549348b02b95840164cdfebeab99b46bcf8def63
startFromBottom <uhh0701@gmail.com>
2020-08-06 12:29:04 +0900

    update Comparator.py

--- Sorting/Comparator.py
+++ Sorting/Comparator.py
@@ -28,23 +28,14 @@ class Player:
             return 0
 
 
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
 
-
-
-if __name__ == "__main__":
-    p1 = Player("aaa", 123)
-    p2 = Player("bbb", 123)
-
-    data = [p1, p2]
-
-# n = int(input())
-# data = []
-# for i in range(n):
-#     name, score = input().split()
-#     score = int(score)
-#     player = Player(name, score)
-#     data.append(player)
-#
-# data = sorted(data, key=cmp_to_key(Player.comparator))
-# for i in data:
-#     print(i.name, i.score)
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
