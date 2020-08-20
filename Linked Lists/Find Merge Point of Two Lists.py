9d7d412f9a12040920d8270b9714762556aa1f50
startFromBottom <uhh0701@gmail.com>
2020-08-20 14:02:20 +0900

    add Find Merge Point of Two Lists.py

---
+++ Linked Lists/Find Merge Point of Two Lists.py
@@ -0,0 +1,116 @@
+"""
+
+problem link : https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
+
+"""
+
+# !/bin/python3
+
+
+import os
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+# Complete the findMergeNode function below.
+
+def node_length(head: SinglyLinkedListNode) -> int:
+    l = 0
+    while head:
+        head = head.next
+        l += 1
+    return l
+
+
+def advance(head: SinglyLinkedListNode, steps: int) -> SinglyLinkedListNode:
+    for _ in range(steps):
+        head = head.next
+    return head
+
+
+def findMergeNode(head1, head2):
+    l1 = node_length(head1)
+    l2 = node_length(head2)
+
+    if l1 > l2:
+        head1 = advance(head1, l1 - l2)
+    else:
+        head2 = advance(head2, l2 - l1)
+    while head1 != head2:
+        head1 = head1.next
+        head2 = head2.next
+    return head1.data
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        index = int(input())
+
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        ptr1 = llist1.head
+        ptr2 = llist2.head
+
+        for i in range(llist1_count):
+            if i < index:
+                ptr1 = ptr1.next
+
+        for i in range(llist2_count):
+            if i != llist2_count - 1:
+                ptr2 = ptr2.next
+
+        ptr2.next = ptr1
+
+        result = findMergeNode(llist1.head, llist2.head)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
