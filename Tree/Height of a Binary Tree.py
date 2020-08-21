1a5586aa236b76cbe8ebc98584db4810c7e3aec1
startFromBottom <uhh0701@gmail.com>
2020-08-21 16:57:08 +0900

    add Height of a Binary Tree.py

---
+++ Tree/Height of a Binary Tree.py
@@ -0,0 +1,72 @@
+"""
+
+problem link : https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=trees
+
+"""
+from collections import deque
+
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+def height(root):
+    q = deque([root])
+    height = -1
+    while q:
+        height += 1
+        for _ in range(len(q)):
+            node = q.pop()
+            if node.left is not None:
+                q.appendleft(node.left)
+            if node.right is not None:
+                q.appendleft(node.right)
+    return height
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
